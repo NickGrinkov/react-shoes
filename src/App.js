24342d2807f391161c08ccdd03df8a1cf2c3a84e
NickGrinkov <nikitagr1801@gmail.com>
2021-11-22 22:35:01 +0300

    created Cart component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
 import React from "react";
 import "./App.scss";
-import Logo from "./img/logo.png";
-import Cart from "./img/cart.svg";
-import User from "./img/user.svg";
-import AddBtn from "./img/plus.svg";
-import cardImg from "./img/shoes/1.jpg";
 
 function App() {
   return (
     <div className="wrapper">
+      <div className="overlay">
+        <div className="cart">
+          <div className="cart__top">
+          <h3>Корзина</h3>
+          <img
+                className="remove-btn"
+                src="/img/btn-remove.svg"
+                alt="remove"
+              />
+          </div>
+          <div className="items">
+            <div className="cart__item">
+              <img width={70} height={70} src="/img/shoes/1.jpg" alt="shoes" />
+              <div className="cart__text">
+                <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+                <b>12 999 р.</b>
+              </div>
+              <img
+                className="remove-btn"
+                src="/img/btn-remove.svg"
+                alt="remove"
+              />
+            </div>
+            <div className="cart__item">
+              <img width={70} height={70} src="/img/shoes/2.jpg" alt="shoes" />
+              <div className="cart__text">
+                <p>Мужские кроссовки Nike Blazer Mid Suede</p>
+                <b>12 999 р.</b>
+              </div>
+              <img
+                className="remove-btn"
+                src="/img/btn-remove.svg"
+                alt="remove"
+              />
+            </div>
+          </div>
+          <div className="cart__total">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21498 руб.</b>
+              </li>
+              <li>
+              <span>Налог 5%</span>
+                <div></div>
+                <b>1074 руб.</b>
+              </li>
+            </ul>
+            <button className="main-button">
+              <span>Оформить заказ</span>
+              <img src="/img/arrow.svg" alt="arrow" /> </button>
+          </div>
+        </div>
+      </div>
       <header className="header">
         <div className="header__left">
-          <img width={40} height={40} src={Logo} alt="logo" />
+          <img width={40} height={40} src="/img/logo.png" alt="logo" />
           <div>
             <h3>React Shoes</h3>
             <p>Лучший магазин кроссовок</p>
@@ -19,17 +69,41 @@ function App() {
         </div>
         <div className="header__right">
           <div>
-            <img width={20} height={20} src={Cart} alt="cart" />
+            <img width={20} height={20} src="/img/cart.svg" alt="cart" />
             <span>1200 руб.</span>
           </div>
-          <img width={20} height={20} src={User} alt="user" />
+          <img width={20} height={20} src="/img/user.svg" alt="user" />
         </div>
       </header>
-      <main>
-        <h1>Все кроссовки</h1>
+      <main className="main">
+        <div className="main__header">
+          <h1>Все кроссовки</h1>
+          <div className="search">
+            <img src="/img/search.svg" alt="search" />
+            <input type="text" placeholder="Поиск..." />
+          </div>
+        </div>
         <div className="shoes">
           <div className="card">
-            <img width={133} height={112} src={cardImg} alt="shoes" />
+            <div className="favorite">
+              <img className="" src="/img/unliked.svg" alt="heart" />
+            </div>
+            <img width={133} height={112} src="/img/shoes/1.jpg" alt="shoes" />
+            <p>
+              Мужские кроссовки <br /> Nike Blazer Mid Suede
+            </p>
+            <div className="card__bottom">
+              <div className="card__price">
+                <span>Цена:</span>
+                <b>12990 руб.</b>
+              </div>
+              <div className="add">
+                <img src="/img/btn-plus.svg" alt="add-btn" />
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <img width={133} height={112} src="/img/shoes/2.jpg" alt="shoes" />
             <p>
               Мужские кроссовки <br /> Nike Blazer Mid Suede
             </p>
@@ -39,12 +113,12 @@ function App() {
                 <b>12990 руб.</b>
               </div>
               <button>
-                <img width={17} height={17} src={AddBtn} alt="add-btn" />
+                <img width={17} height={17} src="/img/plus.svg" alt="add-btn" />
               </button>
             </div>
           </div>
           <div className="card">
-            <img width={133} height={112} src={cardImg} alt="shoes" />
+            <img width={133} height={112} src="/img/shoes/3.jpg" alt="shoes" />
             <p>
               Мужские кроссовки <br /> Nike Blazer Mid Suede
             </p>
@@ -54,12 +128,12 @@ function App() {
                 <b>12990 руб.</b>
               </div>
               <button>
-                <img width={17} height={17} src={AddBtn} alt="add-btn" />
+                <img width={17} height={17} src="/img/plus.svg" alt="add-btn" />
               </button>
             </div>
           </div>
           <div className="card">
-            <img width={133} height={112} src={cardImg} alt="shoes" />
+            <img width={133} height={112} src="/img/shoes/4.jpg" alt="shoes" />
             <p>
               Мужские кроссовки <br /> Nike Blazer Mid Suede
             </p>
@@ -69,12 +143,12 @@ function App() {
                 <b>12990 руб.</b>
               </div>
               <button>
-                <img width={17} height={17} src={AddBtn} alt="add-btn" />
+                <img width={17} height={17} src="/img/plus.svg" alt="add-btn" />
               </button>
             </div>
           </div>
           <div className="card">
-            <img width={133} height={112} src={cardImg} alt="shoes" />
+            <img width={133} height={112} src="/img/shoes/5.jpg" alt="shoes" />
             <p>
               Мужские кроссовки <br /> Nike Blazer Mid Suede
             </p>
@@ -84,12 +158,12 @@ function App() {
                 <b>12990 руб.</b>
               </div>
               <button>
-                <img width={17} height={17} src={AddBtn} alt="add-btn" />
+                <img width={17} height={17} src="/img/plus.svg" alt="add-btn" />
               </button>
             </div>
           </div>
           <div className="card">
-            <img width={133} height={112} src={cardImg} alt="shoes" />
+            <img width={133} height={112} src="/img/shoes/6.jpg" alt="shoes" />
             <p>
               Мужские кроссовки <br /> Nike Blazer Mid Suede
             </p>
@@ -99,22 +173,7 @@ function App() {
                 <b>12990 руб.</b>
               </div>
               <button>
-                <img width={17} height={17} src={AddBtn} alt="add-btn" />
-              </button>
-            </div>
-          </div>
-          <div className="card">
-            <img width={133} height={112} src={cardImg} alt="shoes" />
-            <p>
-              Мужские кроссовки <br /> Nike Blazer Mid Suede
-            </p>
-            <div className="card__bottom">
-              <div className="card__price">
-                <span>Цена:</span>
-                <b>12990 руб.</b>
-              </div>
-              <button>
-                <img width={17} height={17} src={AddBtn} alt="add-btn" />
+                <img width={17} height={17} src="/img/plus.svg" alt="add-btn" />
               </button>
             </div>
           </div>
